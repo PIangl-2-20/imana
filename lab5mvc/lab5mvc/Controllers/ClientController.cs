6b9455777a5afc1a8cc4b77b0ae4a77e98d4f2e6
ImanSt <imanstalbekov@gmail.com>
2022-05-20 09:43:33 +0600

    lab5

---
+++ lab5mvc/lab5mvc/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lab5mvc.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ lab5mvc/lab5mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab5mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
